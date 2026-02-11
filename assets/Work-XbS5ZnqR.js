faa8c4c32271313092bdc741c098d97fa8977ac8
Ricky <rmanyariaquino@gmail.com>
2026-02-10 23:02:28 -0500

    Updates

--- assets/Work-XbS5ZnqR.js
+++ assets/Work-XbS5ZnqR.js
@@ -1,4 +1,4 @@
-import{c as s,j as e}from"./index-DUPvkW3y.js";/**
+import{c as s,j as e}from"./index-IIPhje_u.js";/**
  * @license lucide-react v0.563.0 - ISC
  *
  * This source code is licensed under the ISC license.
